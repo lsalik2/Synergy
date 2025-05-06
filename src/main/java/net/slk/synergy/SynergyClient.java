91654d8d9dacfffe82a0db244211be3eea2e4b78
ohhGHST <salikluis@outlook.com>
2025-05-06 00:29:02 -0500

    Initial commit

---
+++ src/main/java/net/slk/synergy/SynergyClient.java
@@ -0,0 +1,10 @@
+package net.slk.synergy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SynergyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
